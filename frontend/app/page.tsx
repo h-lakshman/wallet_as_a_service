31b11c0440aeb76b4c0617bc130982258a700f80
h-lakshman <lakshman.vaibhav007@gmail.com>
2025-03-30 17:27:05 +0530

    updated lannding page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import HeroSection from "./components/HeroSection";
+import FeaturesSection from "./components/FeaturesSection";
+import CTASection from "./components/CTASection";
+
 export default function Home() {
-  return <div>landing page</div>;
+  return (
+    <main>
+      <HeroSection />
+      <FeaturesSection />
+      <CTASection />
+    </main>
+  );
 }
